823d49876fedf7699b637da5869aac58581ae005
root <root@ip-172-31-34-123.ap-south-1.compute.internal>
2025-09-03 16:09:34 +0000

    jjj

---
+++ src/main/java/com/FlipkartApplication.java
@@ -0,0 +1,20 @@
+package com.flipkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FlipkartApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipkartApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FlipkartApplication.class);
+    }
+}
+
